1d1da04d66f245693bb3b24858dbab71bd627654
Gollapudi Sasank <102504774+Sasank-2004@users.noreply.github.com>
2022-04-11 11:02:10 +0530

    Update code.py

--- codes/code.py
+++ codes/code.py
@@ -1,5 +1,5 @@
 import numpy as np
-import random
+from  random import randint 
 def Determinant(M):
     x = 0
     if len(M)==2:
@@ -10,11 +10,11 @@ def Determinant(M):
             R = np.delete(N,i,axis=1)
             x =x + pow(-1,i)*Determinant(R)*M[0][i] 
         return x
-a = random.randint(0,100)
-b = random.randint(0,100)
-c = random.randint(0,100)
-x = random.randint(0,100)
-y = random.randint(0,100)
-z = random.randint(0,100)
+a = randint(0,100)
+b = randint(0,100)
+c = randint(0,100)
+x = randint(0,100)
+y = randint(0,100)
+z = randint(0,100)
 M = np.array([[a,b,c],[a+2*x,b+2*y,c+2*z],[x,y,z]])
 print(f'|M| = {Determinant(M)}')
