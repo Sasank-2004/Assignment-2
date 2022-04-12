21e1a63344b5f3c40510b7a2e403c6e4c467a449
Gollapudi Sasank <102504774+Sasank-2004@users.noreply.github.com>
2022-04-12 07:14:22 +0530

    Update code.py

--- codes/code.py
+++ codes/code.py
@@ -1,15 +1,5 @@
 import numpy as np
 from  random import randint 
-def Determinant(M):
-    x = 0
-    if len(M)==2:
-        return M[0][0]*M[1][1]-M[0][1]*M[1][0]
-    else :
-        for i in range(len(M)):
-            N = np.delete(M,0,axis=0)
-            R = np.delete(N,i,axis=1)
-            x =x + pow(-1,i)*Determinant(R)*M[0][i] 
-        return x
 a = randint(0,100)
 b = randint(0,100)
 c = randint(0,100)
@@ -17,4 +7,5 @@ x = randint(0,100)
 y = randint(0,100)
 z = randint(0,100)
 M = np.array([[a,b,c],[a+2*x,b+2*y,c+2*z],[x,y,z]])
-print(f'|M| = {Determinant(M)}')
+det = np.linalg.det(M)
+print(f'|M| = {det}')
